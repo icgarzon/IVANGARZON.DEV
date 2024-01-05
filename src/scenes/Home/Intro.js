79d958e30f520716f7a32b538748f1756e020000
Ivan Carron <icgarzon@servicios.in>
2024-01-05 23:26:31 +1000

    Remove Particles

--- src/scenes/Home/Intro.js
+++ src/scenes/Home/Intro.js
@@ -1,32 +1,10 @@
 import React from "react";
 import TextLoop from "react-text-loop";
-import Particles from "react-particles-js";
 import { Container } from "react-grid-system";
 
 function Intro() {
     return (
         <section className="section section-hero section-hero-1 overlay-image" style={{ backgroundImage: `url(/assets/images/hero/1-1.jpg)` }}>
-            <Particles
-                params={{
-                    particles: {
-                        number: {
-                            value: 120,
-                        },
-                        size: {
-                            value: 3,
-                        },
-                    },
-                    interactivity: {
-                        events: {
-                            onhover: {
-                                enable: true,
-                                mode: "repulse",
-                            },
-                        },
-                    },
-                }}
-                className="el-particles"
-            />
             <div className="display-center">
                 <Container>
                     <div className="el-heading">
